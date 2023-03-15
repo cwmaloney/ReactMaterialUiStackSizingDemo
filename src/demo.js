93013a3b9448e06e1bd2bc1a06a6586e9a31cb97
Chris Maloney <chris.w.maloney@gmail.com>
2023-03-15 08:25:25 -0500

    Initial commit

---
+++ src/demo.js
@@ -0,0 +1,154 @@
+import React from "react";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+// import Paper from "@material-ui/core/Paper";
+import Box from "@material-ui/core/Box";
+import Stack from "@material-ui/core/Stack";
+import Typography from "@material-ui/core/Typography";
+
+const styles = {
+  page: {
+    height: "97vh",
+    border: "8px solid pink"
+  },
+  stack: {
+    height: "100%",
+    border: "8px solid red"
+  },
+  box: {
+    //height: "100%",
+    flexGrow: 1,
+    border: "8px solid blue"
+  },
+  header: {
+    display: "hidden",
+    border: "8px solid cornflowerblue"
+  },
+  tablewrapper: {
+    height: "100%",
+    overflowY: "scroll",
+    border: "8px solid black"
+  },
+  tableContainer: {
+    height: "100%",
+    border: "8px solid green"
+  },
+  table: {
+    border: "8px solid navy",
+    stickyHeader: true
+  },
+  tableBody: {},
+  row: {
+    height: "100px",
+    border: "8px solid green"
+  }
+};
+
+export default function BasicTable() {
+  return (
+    <Box sx={styles.page}>
+      <Stack sx={styles.stack}>
+        <Typography sx={styles.header}>
+          line 1
+          <br />
+          line 2
+          <br />
+        </Typography>
+        <Box sx={styles.box}>
+          <Stack sx={styles.stack}>
+            <Typography sx={styles.header}>
+              line 1
+              <br />
+            </Typography>
+            <Box sx={styles.box}>
+              <Stack sx={styles.stack}>
+                <Typography sx={styles.header}>
+                  line 1
+                  <br />
+                  line 2
+                  <br />
+                </Typography>
+                <Box sx={styles.tablewrapper}>
+                  <Typography sx={styles.header}>
+                    line 1
+                    <br />
+                    line 2
+                    <br />
+                  </Typography>
+                  <Box sx={styles.row}>content 1</Box>
+                  <Box sx={styles.row}>content 2</Box>
+                  <Box sx={styles.row}>content 3</Box>
+                  <Box sx={styles.row}>content 4</Box>
+                  <Box sx={styles.row}>content 5</Box>
+                  <Box sx={styles.row}>content 6</Box>
+                  <Box sx={styles.row}>content 7</Box>
+                  {/* <TableContainer sx={styleClasses.tableContainer}>
+                    <Table
+                      stickyHeader
+                      sx={styleClasses.table}
+                      aria-label="simple table"
+                    >
+                      <TableHead>
+                        <TableRow>
+                          <TableCell>Dessert (100g serving)</TableCell>
+                          <TableCell align="right">Calories</TableCell>
+                          <TableCell align="right">Fat&nbsp;(g)</TableCell>
+                          <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+                          <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                        </TableRow>
+                      </TableHead>
+                      <TableBody sx={styleClasses.tableBody}>
+                        {rows.map((row) => (
+                          <TableRow key={row.name}>
+                            <TableCell component="th" scope="row">
+                              {row.name}
+                            </TableCell>
+                            <TableCell align="right">{row.calories}</TableCell>
+                            <TableCell align="right">{row.fat}</TableCell>
+                            <TableCell align="right">{row.carbs}</TableCell>
+                            <TableCell align="right">{row.protein}</TableCell>
+                          </TableRow>
+                        ))}
+                      </TableBody>
+                    </Table>
+                  </TableContainer> */}
+                </Box>
+              </Stack>
+            </Box>
+          </Stack>
+        </Box>
+      </Stack>
+    </Box>
+  );
+}
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData("Frozen yogurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9),
+  createData("Eclair2", 262, 16.0, 24, 6.0),
+  createData("Cupcake2", 305, 3.7, 67, 4.3),
+  createData("Gingerbread2", 356, 16.0, 49, 3.9),
+  createData("Frozen yoghurt2", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich2", 237, 9.0, 37, 4.3),
+  createData("Eclair3", 262, 16.0, 24, 6.0),
+  createData("Cupcake3", 305, 3.7, 67, 4.3),
+  createData("Frozen yoghurt3", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich3", 237, 9.0, 37, 4.3),
+  createData("Eclair4", 262, 16.0, 24, 6.0),
+  createData("Cupcake4", 305, 3.7, 67, 4.3),
+  createData("Frozen yoghurt4", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich4", 237, 9.0, 37, 4.3),
+  createData("Eclair5", 262, 16.0, 24, 6.0),
+  createData("Cupcake5", 305, 3.7, 67, 4.3)
+];
